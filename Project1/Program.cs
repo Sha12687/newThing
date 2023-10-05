ba502f26f97740b04e1e159ce8c34347f6aeec64
Shashant <shashantchaudhary999@gmail.com>
2023-10-05 15:31:09 +0530

    Add project files.

---
+++ Project1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        int i; bool flag;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Program p = new  Project1.Program();
+            Console.WriteLine($"value of is : {p.i}");
+            Console.WriteLine($"value of p is :{p.flag}");
+            Console.ReadLine();
+        }
+    }
+}
